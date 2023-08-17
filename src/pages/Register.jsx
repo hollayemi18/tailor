d4d29e05d1230a51a9ced3e41fb10cfed2f586d2
hollayemi18 <olayemio521@gmail.com>
2023-08-17 07:55:03 +0100

    mint

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,57 +1,56 @@
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { useNavigate } from "react-router-dom";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 //import { PrimaryButton, SecondaryButton } from "../layouts/buttons";
 import { Checkbox, Input } from "../layouts/field";
-import axios from 'axios'
-import  {toast ,ToastContainer} from 'react-toastify'
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
 
 const Register = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-      const register= async (e)=>{
-    
-  e.preventDefault()
-         if(!email ){
-         return toast.error("All fields are required")
-      }
-         if(!username ){ 
-         return toast.error("All fields are required")
-      }
-           if(!password ){
-         return toast.error("All fields are required")
-      }
-    try {  
-         const reg = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/register`,{
-        username,email,password
-      },{withCredentials: true})
-      console.log(reg)     
-   
-      if(reg){
-        navigate('/login')
-       return toast.success("successfully registered")
-      }
-      if(!reg){
-    return toast.error("User Already Exist")
-
-      }
-      
-    } catch (error) {
-    return toast.error("Something went wrong")
-    }
-  }
+    // const [reg, setReg] = useState("");
+    const register = async e => {
+        e.preventDefault();
+        if (!email) {
+            return toast.error("All fields are required");
+        }
+        if (!username) {
+            return toast.error("All fields are required");
+        }
+        if (!password) {
+            return toast.error("All fields are required");
+        }
+        await axios
+            .post(
+                "http://localhost:8080/register",
+                {
+                    username,
+                    email,
+                    password
+                },
+                { withCredentials: true }
+            )
+            .then(reg => {
+                toast.success("successfully registered");
+                navigate("/login");
+            })
+            .catch(error => {
+                return toast.error("something went wrong");
+            });
+    };
     return (
-         <div className="flex flex-col gap-2 mx-10 mb-96 md:mx-72 pt-10">
+        <div className="flex flex-col gap-2 mx-10 mb-96 md:mx-72 pt-10">
             <h3 className="text-center text-xl font-semibold text-black">Create New Account</h3>
             <p className="text-center text-sm mt-2 mb-10">
                 start an adventure of a lifetime of fashion sense
             </p>
-            
-            <form className="space-y-5"  onSubmit={register}>
+
+            <form className="space-y-5" onSubmit={register}>
                 <ToastContainer />
                 <div>
                     <Input
@@ -61,7 +60,6 @@ const Register = () => {
                         placeholder="Enter username"
                         value={username}
                         onChange={e => setUsername(e.target.value)}
-                    
                     />
                 </div>
 
@@ -73,7 +71,6 @@ const Register = () => {
                         placeholder="Enter email"
                         value={email}
                         onChange={e => setEmail(e.target.value)}
-                        
                     />
                 </div>
 
@@ -85,7 +82,6 @@ const Register = () => {
                         placeholder="Enter password"
                         value={password}
                         onChange={e => setPassword(e.target.value)}
-                        
                     />
                 </div>
 
@@ -93,10 +89,10 @@ const Register = () => {
                     <Checkbox id="remember" label="I agree to privacy policy & terms" />
                 </div>
 
-                <button 
-                 className="text-white transition-all duration-300 px-2 border py-3 rounded-md border-gray-300 w-full flex justify-center items-center space-x-2 bg-indigo-500 hover:bg-gray-300"
-                onSubmit={register}>
-                    
+                <button
+                    className="text-white transition-all duration-300 px-2 border py-3 rounded-md border-gray-300 w-full flex justify-center items-center space-x-2 bg-indigo-500 hover:bg-gray-300"
+                    onSubmit={register}
+                >
                     <span>Sign up</span>
                 </button>
 
@@ -107,8 +103,7 @@ const Register = () => {
                 </div>
 
                 <div className="flex items-center space-x-4 lg:space-x-2 xl:space-x-4 text-sm font-semibold">
-                    <button 
-                    className="transition-all duration-300 px-2 border py-3 rounded-md border-gray-300 w-full flex justify-center items-center space-x-2 hover:bg-gray-300">
+                    <button className="transition-all duration-300 px-2 border py-3 rounded-md border-gray-300 w-full flex justify-center items-center space-x-2 hover:bg-gray-300">
                         <FcGoogle className="h-5 w-5 lg:w-4 lg:h-4 xl:h-5 xl:w-5" />
 
                         <span className="text-[0.7rem] md:text-sm lg:text-[0.7rem] xl:text-sm">
