62bd1f6f367d357a5f948e646ea9cf60de4ce428
hollayemi18 <olayemio521@gmail.com>
2023-09-25 17:17:57 +0100

    d

--- src/clientSide/component/layouts/Navbar.jsx
+++ src/clientSide/component/layouts/Navbar.jsx
@@ -11,25 +11,7 @@ const bell = <FontAwesomeIcon icon={faBell} size='xl' />;
 
 const Navbar = () => {
   //const [logging, setLogging] = useState(false);
-  const [username, setUsername] = useState('');
-
-  useEffect(() => {
-    const local = process.env.REACT_APP_DEV_MODE;
-    const fetchData = async () => {
-      try {
-        const result = await axios.get(`${local}auth/user`, {
-          withCredentials: true,
-        });
-        const data = result.data;
-        if (data) {
-          setUsername(data);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
+  const [username, setUsername] = useState('user');
 
   return (
     <div className=' flex flex-row pt-0 mx-1 justify-between lg:justify-evenly py-0xl:py-0 border-b-1 '>
